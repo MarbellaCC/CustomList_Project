fac3928a59ad45858692a2cc22d1c89f7d5a22b6
unknown <camposmarbella898029@ymail.com>
2021-01-25 12:08:17 -0600

    https://github.com/MarbellaCC/CustomList_Project

---
+++ AddMethodTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AddMethodTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AddMethodTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("495854b2-ed04-412c-957c-7a9b56130ec4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AddMethodTest/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Custom_List;
+
+namespace AddMethodTest
+{
+    [TestClass]
+    public class AddMethodTest
+    {
+        [TestMethod]
+        public void AddItemToCustomList_ChekCountEquals()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            string color = "periwinkle";
+            int actual;
+            int expected = 1;
+
+            //Act
+            list.Add(color);
+            actual = list.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddItemToCustomList_ChekAtIndex0()
+        {
+            //Arange
+            CustomList list = new CustomList();
+            string expected = "periwinkel";
+            string actual;
+
+            //Act
+            list.Add(expected);
+            actual = list[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    
+        [TestMethod]
+        public void AddItemsToCustomList_CheckForItemAtIndex3()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            string color1 = "azure";
+            string color2 = "royal blue";
+            string color3 = "turquoise";
+            string color4 = "periwinkle";
+            string expected = "periwinkle";
+            string actual;
+
+            //Act
+            list.Add(color1);
+            list.Add(color2);
+            list.Add(color3);
+            list.Add(color4);
+
+            actual = list[3];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+        
+    }
+
+}
