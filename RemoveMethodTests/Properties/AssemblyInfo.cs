44586f5de6f2f3af812273823cc335d60f8ea325
unknown <camposmarbella898029@ymail.com>
2021-01-26 10:17:14 -0600

    Added remove method tests

---
+++ RemoveMethodTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RemoveMethodTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RemoveMethodTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e6133be-e097-46ab-a7a1-0f2a59e232bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RemoveMethodTests/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Custom_List;
+
+namespace RemoveMethodTests
+{
+    [TestClass]
+    public class RemoveMethodTest
+    {
+        [TestMethod]
+        public void RemoveItemFromCustomList_CheckCountEquals()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            string color1 = "periwinkle";
+            string color2 = "azure";
+            int actual;
+            int expected = 1;
+
+            //Act
+            list.Add(color1, color2);
+            list.Remove(color2);
+            actual = list.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        
+        }
+
+        [TestMethod]
+        public void RemoveItemFromCustomList_CheckAtIndex0()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            string color = "periwinkle";
+            string expected = null;
+            string actual;
+
+            //Act
+            list.Add(expected);
+            list.Remove(expected);
+            actual = list[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveItemFromCustomList_CheckAtIndex1()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            string color1 = "azure";
+            string color2 = "royal blue";
+            string expected = "turquoise";
+            string actual;
+
+            //Act
+            list.Add(color1, color2, expected);
+            list.Remove(color2);
+            actual = list[1];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            
+        }
+
+        [TestMethod]
+        public void RemoveIntItemFromCustomList_CheckCount()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            int num1 = 32;
+            int num2 = 99;
+            int expected = 1;
+            int actual;
+
+            //Act
+            list.Add(num1, num2);
+            list.Remove(num2);
+            actual = list.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveIntItemFromCustomList_CheckAtIndex0()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            int num1 = 32;
+            int expected = 99;
+            int actual;
+
+            //Act
+            list.Add(num1, expected);
+            list.Remove(num1);
+            actual = list[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
