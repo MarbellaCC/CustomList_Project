c2d1fa4bb69915f70a41cab74a797356f3870697
unknown <camposmarbella898029@ymail.com>
2021-01-25 22:45:47 -0600

    added check new capacity test method

--- AddMethodTest/UnitTest1.cs
+++ AddMethodTest/UnitTest1.cs
@@ -64,8 +64,89 @@ namespace AddMethodTest
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public void AddItemToCustomList_CheckIntCanBeAdded()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            int myNumber = 24;
+            
+            int expected = 1;
+            int actual;
+
+            //Act
+            list.Add(myNumber);
+            actual = list.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddItemToCustomList_CheckIntAtIndex0()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            int expected = 24;
+            int actual;
+
+            //Act
+            list.Add(expected);
+            actual = list[0];
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
         
-        
+        [TestMethod]
+        public void AddItemsToCustomList_CheckNewCapacity()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            string color1 = "azure";
+            string color2 = "royal blue";
+            string color3 = "turquoise";
+            string color4 = "periwinkle";
+            string color5 = "teal";
+            string color6 = "evergreen";
+            string color7 = "cerulean";
+            
+            int expected = 8;
+            int actual;
+            
+            //Act
+            list.Add(color1);
+            list.Add(color2);
+            list.Add(color3);
+            list.Add(color4);
+            list.Add(color5);
+            list.Add(color6);
+            list.Add(color7);
+            
+            actual = list.Capacity;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddItemsToCustomList_CheckOldListGone()
+        {
+
+        }
+
+        [TestMethod]
+        public void AddItemsToCustomList_CheckValuesStoredSomewhere()
+        {
+
+        }
+
+        [TestMethod]
+        public void AddItemsToCustomList_CheckValuesAreStoredInNewList()
+        {
+
+        }
     }
 
 }
